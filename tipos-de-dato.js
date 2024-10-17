1b05ecf49a889847cd4728538f2e44a3652034f6
Jesus <jsusgl999@gmail.com>
2024-10-16 18:15:31 -0600

    Tarea ya realizaada

---
+++ tipos-de-dato.js
@@ -0,0 +1,46 @@
+/* 
+Agrega uno o varios ejemplos para los siguientes valores y que se imprima a que tipo de 
+dato pertenecen dichos valores, 
+justo como en el ejemplo anterior. Los valores son: ",
+  - 42,
+  - 'Veinticinco',	
+  - -666,
+  - true,
+  - 0,
+  - '',
+  - null,
+  - undefined,
+  - FALSE,- Agrega tus propios ejemplos y experimenta con algunos valores y sus tipos d datos.",
+  
+Puntos extra 
+
+  - 5 === 5;
+  - "Hola mundo".length
+  - {}
+
+  */
+
+
+console.log(6 * 7)
+
+console.log("'Veinti" + "cinco'");
+
+console.log(-666)
+
+console.log(true)
+
+console.log(0)
+
+console.log("''");
+
+console.log(null);
+
+let x; console.log(x);
+
+console.log("FAL" + "SE");
+
+console.log("5 === 5;");
+
+console.log("Hola mundo".length);
+
+console.log("{}");
